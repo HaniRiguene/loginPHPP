89ba77e9790a8391388cad677a182f83dd8b4acc
Riguene Hani <84547492+HaniRiguene@users.noreply.github.com>
2022-05-11 00:13:06 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,3 +1,88 @@
-<?php
+<!-- generate dashboard admin -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">                                                      
+    <title>Hani</title>
+    <link rel="stylesheet" href="../css/style.css">
+    <link rel="stylesheet" href="../css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css/font-awesome.css">
+    <link rel="stylesheet" href="../css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css/font-awesome-ie7.min.css">
+    <link rel="stylesheet" href="../css/font-awesome-ie7.css">
+    <link rel="stylesheet" href="../css/font-awesome-ie7.min.css">
 
-include("layout.php");
+    <script src="../js/jquery.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/script.js"></script>
+    <script src="../js/jquery.min.js"></script>
+    <script src="../js/jquery.js"></script>
+    
+</head>
+<body>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="header">
+                    <div class="logo">
+                        <img src="../images/logo.png" alt="">
+                    </div>
+                    <div class="nav">
+                        <ul>
+                            <li><a href="index.php">Home</a></li>
+                            <li><a href="index.php">About</a></li>
+                            <li><a href="index.php">Contact</a></li>
+                            <li><a href="index.php">Login</a></li>
+                        </ul>                           
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="content">
+                    <div class="content-header">
+                        <h1>Welcome to Hani</h1>
+                    </div>
+                    <div class="content-body">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="footer">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-4">
+                                <h3>About Us</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
+                            </div>
+                            <div class="col-md-4">
+                                <h3>Latest Posts</h3>
+                                <ul class="list-unstyled">
+                                    <li><a href="#">Lorem ipsum dolor sit amet</a></li>
+                                    <li><a href="#">Consectetur adipiscing elit</a></li>
+                                    <li><a href="#">Integer molestie lorem at massa</a></li>
+                                    <li><a href="#">Facilisis in pretium nisl aliquet</a></li>
+                                    <li><a href="#">Nulla volutpat aliquam velit
+                                    </a></li>
+                                </ul>
+                            </div>
+                            <div class="col-md-4">
+                                <h3>Contact Us</h3>
+                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae. Sed dui lorem, adipiscing in adipiscing et, interdum nec metus. Mauris ultricies, justo eu convallis placerat, felis enim.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
+<!-- end of generate dashboard admin -->
+
