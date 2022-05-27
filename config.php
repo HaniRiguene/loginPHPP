1ced8cb4d25a32606765e4dc70ddeaf0d23c87ab
Riguene Hani <84547492+HaniRiguene@users.noreply.github.com>
2022-05-27 22:12:08 +0100

    Add files via upload

--- config.php
+++ config.php
@@ -1,5 +1,18 @@
-<?php
-ini_set('display_errors',1);
-ini_set('display_startup_errors',1);
-error_reporting(E_ALL);
+<?Php
+$host_name = "localhost";
+$database = "chart_data"; // Change your database name
+$username = "root";          // Your database user id 
+$password = "";          // Your password
+
+//error_reporting(0);// With this no error reporting will be there
+//////// Do not Edit below /////////
+
+$connection = mysqli_connect($host_name, $username, $password, $database);
+
+if (!$connection) {
+    echo "Error: Unable to connect to MySQL.<br>";
+    echo "<br>Debugging errno: " . mysqli_connect_errno();
+    echo "<br>Debugging error: " . mysqli_connect_error();
+    exit;
+}
 ?>
--- index-csv.php
+++ index-csv.php
@@ -1,40 +1,27 @@
-<!doctype html >
+<!doctype html public "-//w3c//dtd html 3.2//en">
 <html>
 <head>
-<title>HaniChart</title>
+<title>plus2net.com : Pie chart using data from CSV file</title>
 </head>
 <body >
 <?Php
-require "config-pdo.php";// Database connection
-
-if($stmt = $connection->query("SELECT language, nos  FROM chart_data")){
-
-  echo "No of records : ".$stmt->num_rows."<br>";
+$f_pointer=fopen("chart_data.csv","r"); // file pointer
 $php_data_array = Array(); // create PHP array
-  echo "<table>
-<tr> <th>Language</th><th>Nos</th></tr>";
-while ($row = $stmt->fetch_row()) {
-   echo "<tr><td>$row[0]</td><td>$row[1]</td></tr>";
-   $php_data_array[] = $row; // Adding to array
-   }
-echo "</table>";
-}else{
-echo $connection->error;
+while(! feof($f_pointer)){
+$ar=fgetcsv($f_pointer);
+//echo print_r($ar); // print the array 
+$php_data_array[] = $ar; // Adding to array
 }
-//print_r( $php_data_array);
-// You can display the json_encode output here. 
-echo json_encode($php_data_array); 
-
-// Transfor PHP array to JavaScript two dimensional array 
+//print_r($php_data_array);
 echo "<script>
-        var my_2d = ".json_encode($php_data_array)."
+var my_2d=".json_encode($php_data_array)."
 </script>";
 ?>
 
 
 <div id="chart_div"></div>
 <br><br>
-
+<a href=https://www.plus2net.com/php_tutorial/chart-database.php>Pie Chart from MySQL database</a>
 </body>
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 <script>
@@ -47,12 +34,16 @@ echo "<script>
         var data = new google.visualization.DataTable();
         data.addColumn('string', 'language');
         data.addColumn('number', 'Nos');
-		for(i = 0; i < my_2d.length; i++)
-    data.addRow([my_2d[i][0], parseInt(my_2d[i][1])]);
+for(i=0;i<my_2d.length;i++)
+data.addRow([my_2d[i][0],parseInt(my_2d[i][1])]);
+
 // above row adds the JavaScript two dimensional array data into required chart format
-    var options = {title:'plus2net.com : How the tutorials are distributed',
-                       width:600,
-                       height:500};
+    var options = {title:'plus2net.com :Nos of tutorials',
+           width:500,
+           height:500,
+		   legend:'left',
+           is3D:true,
+		   };
 
         // Instantiate and draw the chart
         var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
@@ -60,10 +51,3 @@ echo "<script>
       }
 </script>
 </html>
-
-
-
-
-
-
-
